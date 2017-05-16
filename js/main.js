a18dbe2d9e5b6d3eb605bfa9f6e3d7e8a2c3052e
martrinity <martiloca_4@hotmail.com>
2017-05-16 01:46:47 -0400

    ejercicio guiado-first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 function agregaPosteo() {
-//escribe aca el codigo
+var containerPosteos = document.getElementById("contenedor-posteos");
+var nombreFF = document.getElementById("nombre").value = "";
+var contenidoFF = document.getElementById("cajaposteos").value = "";
+var nuevoPost = document.createElement("div");
+
+var contenedorNombre = document.createElement("strong");
+var contenedorPost = document.createElement("p");
+var separacion = document.createElement("hr");
+
+var nodoNombre = document.createTextNode(nombreFF + " escribió:");
+var nodoPosteo = document.createTextNode(contenidoFF);
+
+contenedorNombre.appendChild(nodoNombre);
+contenedorPost.appendChild(nodoPosteo);
+
+nuevoPost.appendChild(contenedorNombre);
+nuevoPost.appendChild(contenedorPost);
+nuevoPost.appendChild(separacíon);
+
+nuevoPost.setAttribute("class", "posteo");
+containerPosteos.appendChild(nuevoPost);
 }
+
+i.addEventListener("click", function(){
+	i.classList.toggle('rojo');
+});
+
